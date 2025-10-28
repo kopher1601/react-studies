61665cced9a884ca37f1da30ab30c61d7c52fa7d
kopher <kopher1601@gmail.com>
2025-10-29 01:22:30 +0900

    Integrate Shadcn/ui, configure alias paths, and update TailwindCSS setup with additional plugins and global styles.

--- kopher-sns/README.md
+++ kopher-sns/README.md
@@ -5,3 +5,5 @@
 - Vite
 - React
 - TypeScript
+- Shadcn/ui
+
--- kopher-sns/src/App.tsx
+++ kopher-sns/src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import { Button } from "@/components/ui/button.tsx";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Button>Hello</Button>
+    </div>
+  );
 }
 
 export default App;
