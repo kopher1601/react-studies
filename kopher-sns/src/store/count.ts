8ef022e8dfaec54e0bb99d29e4158e13ea8dc0a3
kopher <kopher1601@gmail.com>
2025-10-29 18:21:47 +0900

    Add Zustand middlewares for persistence, devtools, and subscription handling in count store

--- kopher-sns/src/store/count.ts
+++ kopher-sns/src/store/count.ts
@@ -1,23 +1,56 @@
 import { create } from "zustand";
-import { combine } from "zustand/middleware";
+import {
+  combine,
+  createJSONStorage,
+  devtools,
+  persist,
+  subscribeWithSelector,
+} from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
 
 const useCountStore = create(
-  immer(
-    combine(
+  devtools(
+    persist(
+      // 브라우저의 localStorage 에 state 를 보관하게 해줌
+      subscribeWithSelector(
+        immer(
+          combine(
+            {
+              count: 0,
+            },
+            (set) => ({
+              increment: () => {
+                set((state) => (state.count += 1));
+              },
+              decrement: () => {
+                set((state) => (state.count -= 1));
+              },
+            }),
+          ),
+        ),
+      ),
       {
-        count: 0,
+        name: "count-store",
+        // localStorage 에 보관하고 싶은 값만 지정할 수 있다.
+        partialize: (state) => ({ count: state.count }),
+        // 브라우저의 sessionStorage 에 값을 보관게 지정
+        storage: createJSONStorage(() => localStorage),
       },
-      (set) => ({
-        increment: () => {
-          set((state) => (state.count += 1));
-        },
-        decrement: () => {
-          set((state) => (state.count -= 1));
-        },
-      }),
     ),
+    {
+      name: "count-store", // 브라우저에서 redux dev tools 의 설치가 필요하다
+    },
   ),
+);
+
+// useEffect 와 비슷한 역할을 한다
+useCountStore.subscribe(
+  (state) => state.count,
+  // listener
+  (count, prevCount) => {
+    console.log("count changed", count);
+    console.log("prevCount", prevCount);
+  },
 );
 
 export function useCount() {
