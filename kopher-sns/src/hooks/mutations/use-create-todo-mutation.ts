e767b64912c4cbe2b787606845ca7fccd39ddce5
kopher <kopher1601@gmail.com>
2025-11-04 04:07:21 +0900

    QueryKey Factory

--- kopher-sns/src/hooks/mutations/use-create-todo-mutation.ts
+++ kopher-sns/src/hooks/mutations/use-create-todo-mutation.ts
@@ -1,11 +1,14 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { createTodo } from "@/api/create-todo.ts";
+import { QUERY_KEYS } from "@/lib/constants.ts";
 
 export function useCreateTodoMutation() {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: createTodo,
-    onSuccess: () => {
-      window.location.reload();
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: QUERY_KEYS.todo.list });
     },
   });
 }
--- kopher-sns/src/hooks/quries/use-todo-data-by-id.ts
+++ kopher-sns/src/hooks/quries/use-todo-data-by-id.ts
@@ -1,9 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import { fetchTodoById } from "@/api/fetch-todo-by-id.ts";
+import { QUERY_KEYS } from "@/lib/constants.ts";
 
 export function useTodoDataById(id: string) {
   return useQuery({
     queryFn: () => fetchTodoById(id),
-    queryKey: ["todos", id],
+    queryKey: QUERY_KEYS.todo.detail(id),
   });
 }
