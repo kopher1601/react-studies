9c71713df9013509b7f1adddcfcd5d2094afa587
kopher <kopher1601@gmail.com>
2025-11-04 04:13:50 +0900

    Return Data

--- kopher-sns/src/hooks/mutations/use-create-todo-mutation.ts
+++ kopher-sns/src/hooks/mutations/use-create-todo-mutation.ts
@@ -1,14 +1,18 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { createTodo } from "@/api/create-todo.ts";
 import { QUERY_KEYS } from "@/lib/constants.ts";
+import type { Todo } from "@/types.ts";
 
 export function useCreateTodoMutation() {
   const queryClient = useQueryClient();
 
   return useMutation({
     mutationFn: createTodo,
-    onSuccess: async () => {
-      await queryClient.invalidateQueries({ queryKey: QUERY_KEYS.todo.list });
+    onSuccess: async (newTodo) => {
+      queryClient.setQueryData<Todo[]>(QUERY_KEYS.todo.list, (oldData) => {
+        if (!oldData) return;
+        return [...oldData, newTodo];
+      });
     },
   });
 }
