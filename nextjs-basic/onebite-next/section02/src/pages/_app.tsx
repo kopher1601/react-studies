d98a52528d0bb8d93eec20cb9a6b95cbdc509cee
kopher <kopher1601@gmail.com>
2026-03-01 23:51:55 +0900

    Navigation

--- nextjs-basic/onebite-next/section02/src/pages/_app.tsx
+++ nextjs-basic/onebite-next/section02/src/pages/_app.tsx
@@ -1,9 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Link from "next/link";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <>
-    <header>Global Header</header>
+    <header>
+      <Link href="/">index</Link>
+    </header>
     <Component {...pageProps} />
   </>;
 }
