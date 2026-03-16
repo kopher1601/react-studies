13d21c52008751a2ef71294f0272ac71d3f522cd
kopher <kopher1601@gmail.com>
2026-03-16 23:07:47 +0900

    SSR1

--- nextjs-basic/onebite-next/section02/src/pages/index.tsx
+++ nextjs-basic/onebite-next/section02/src/pages/index.tsx
@@ -2,8 +2,23 @@ import SearchableLayout from "@/components/searchable-layout";
 import styles from "./index.module.css";
 import books from "@/mock/books.json"
 import BookItem from "@/components/book-item";
+import { InferGetServerSidePropsType } from "next";
 
-export default function Home() {
+// 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터를 불러오는 함수
+// 오직 서버측에서 실행되는 함수.
+export const getServerSideProps = async () => {
+  const data = "hello"
+
+  // 반드시 props를 포함하는 객체를 반환해야한다.
+  return {
+    props: {
+      data
+    }
+  }
+}
+
+export default function Home({ data }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  console.log(data)
   return (
     <div className={styles.container}>
       <section>
